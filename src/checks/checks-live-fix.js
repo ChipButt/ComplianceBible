092c0015624e906e7f5555b83c5e5dafebc43ba6
ChipButt <jameschipbutt@hotmail.com>
2026-06-13 23:28:59 +0100

    Delete checks live fix compatibility script

--- src/checks/checks-live-fix.js
+++
@@ -1,7 +0,0 @@
-// Compatibility binding only. Real Checks UI lives in src/checks/area-groups.js.
-(function(){
-  try {
-    if (window.checks) checks = window.checks;
-    if (typeof render === 'function') setTimeout(function(){ render(); }, 0);
-  } catch (_) {}
-})();
