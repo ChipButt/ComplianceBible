985e7835cfe2db6e08abd698ebf375ea685e78a7
ChipButt <jameschipbutt@hotmail.com>
2026-06-20 00:28:24 +0100

    Load modal shim before settings refinements

--- src/settings/settings-hub.js
+++ src/settings/settings-hub.js
@@ -11,5 +11,9 @@
     return script;
   }
   var prototype = load('src/settings/test-settings-page.js?v=20260620-2');
-  prototype.onload = function(){ load('src/settings/test-settings-page-refinements.js?v=20260620-1'); };
+  prototype.onload = function(){
+    window.modalRoot = window.modalRoot || document.getElementById('modal');
+    var refinements = load('src/settings/test-settings-page-refinements.js?v=20260620-2');
+    refinements.onload = function(){ load('src/settings/test-settings-page-touch-fixes.js?v=20260620-1'); };
+  };
 })();
