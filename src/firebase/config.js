78e730b1baed2caee9d1bc85cb4f3e4c1a68c128
ChipButt <jameschipbutt@hotmail.com>
2026-07-01 05:18:34 +0100

    Remove committed Firebase API key from config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,14 @@
 (function firebaseProjectConfig() {
+  // Do not commit real Firebase API keys or project config to this public repository.
+  // For production, inject these values at deploy time or use the Firebase Setup flow only on a trusted setup device.
   window.COMPLIANCE_FIREBASE_CONFIG = window.COMPLIANCE_FIREBASE_CONFIG || {
-    apiKey: 'AIzaSyAc9jEiUNSapQ-kdgNe8DPUd_UPw21ZRrM',
-    authDomain: 'compliancebible.firebaseapp.com',
-    projectId: 'compliancebible',
-    storageBucket: 'compliancebible.firebasestorage.app',
-    messagingSenderId: '194386239844',
-    appId: '1:194386239844:web:9caf48b14f33649a929a71',
-    measurementId: 'G-JMRF00JQG1'
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   };
 
   window.COMPLIANCE_FIREBASE_PUB_ID = window.COMPLIANCE_FIREBASE_PUB_ID || 'piston-club';
